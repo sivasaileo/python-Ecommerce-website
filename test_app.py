be86e6c82c852f9404d3250f2844216778dcd942
sivasaileo <learningpractice999@gmail.com>
2025-03-28 17:07:23 +0530

    modified pipeline

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Welcome to My DevOps Practice App!', response.data)
+
+    def test_orders(self):
+        response = self.app.get('/orders')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Orders', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
